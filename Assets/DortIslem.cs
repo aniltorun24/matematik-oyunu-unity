58808ec4bea751db7f2d6b107796f503fc78fbe6
aniltorun24 <torunanil05@gmail.com>
2024-02-02 14:55:47 +0300

    matematik dört işlem oyunu
    2023 yılının yaz ayında İSUBÜ 1.yaz stajımı yaptığım Yıldız Teknik Üniversitesindeki oyun programlama stajında kendimi geliştirdikten sonra yaptığım ilk oyun

---
+++ Assets/DortIslem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DortIslem : MonoBehaviour
+{
+    public Text ilksayi,ikincisayi,islem,cevap,sonuc;
+    public InputField sonucInput;
+    int sayi1,sayi2,islemIsareti;
+    int islemSonucu;
+    // Start is called before the first frame update
+    void Start()
+    {
+    yeniSoru();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void cevapKontrol(){
+        if(int.Parse(cevap.text)==islemSonucu)
+        sonuc.text="DOĞRU";
+        else{
+            sonuc.text="YANLIŞ";
+        }
+    }
+    public void yeniSoru(){
+        sonuc.text = "";
+        sonucInput.text = "";
+       sayi1 = Random.Range(1,10);
+       sayi2 = Random.Range(1,10); 
+       islemIsareti = Random.Range(1,5);
+       switch(islemIsareti){
+        case 1:
+            islem.text = "+";
+            islemSonucu=sayi1+sayi2;
+            break;
+        case 2:
+            islem.text = "-";
+            islemSonucu=sayi1-sayi2;
+            break;
+        case 3:
+            islem.text = "*";
+            islemSonucu=sayi1*sayi2;
+            break;
+        case 4:
+            islem.text = "/";
+            islemSonucu=sayi1/sayi2;
+            break;
+        }
+        ilksayi.text = sayi1 + "";
+        ikincisayi.text = sayi2 + "";
+       
+    }
+}
